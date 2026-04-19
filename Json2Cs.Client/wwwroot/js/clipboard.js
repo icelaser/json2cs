341c1090a52021333f12c1ac26eaca1ac5f2c675
Sven Buckbesch <s.buckbesch@solarlux.com>
2026-04-19 22:08:40 +0200

    kleinere änderungen

--- Json2Cs.Client/wwwroot/js/clipboard.js
+++ Json2Cs.Client/wwwroot/js/clipboard.js
@@ -15,6 +15,17 @@ window.updateStatusDisplay = (message) => {
     }
 };
 
+window.setCookie = (name, value, days) => {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = name + '=' + encodeURIComponent(value) + ';expires=' + expires.toUTCString() + ';path=/;SameSite=Lax';
+};
+
+window.getCookie = (name) => {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? decodeURIComponent(match[2]) : '';
+};
+
 // Monaco Editor initialization
 window.initializeMonacoEditor = async (containerId, initialValue, language, editable, dotnetRef) => {
     try {
